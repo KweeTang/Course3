093ba2215d6b0ef7c4ec5eabdc52398c8ec82758
Christine Alvarado <cjalvarado@eng.ucsd.edu>
2015-11-04 10:18:45 -0800

    First commit

---
+++ Course3Solutions/src/map/MapNode.java
@@ -0,0 +1,18 @@
+/**
+ * A class to represent a node in the map
+ */
+package map;
+
+import java.util.Set;
+
+/**
+ * @author Christine
+ *
+ */
+public class MapNode 
+{
+	/** The list of edges out of this node */
+	private Set<MapEdge> edges;
+	
+	
+}
