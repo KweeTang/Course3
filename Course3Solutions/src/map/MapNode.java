845a3fdde0bf42e74f03a622ac26d0355cf38284
Christine Alvarado <cjalvarado@eng.ucsd.edu>
2015-11-11 13:04:10 -0800

    Additions to MapEdge and MapNode in map

--- Course3Solutions/src/map/MapNode.java
+++ Course3Solutions/src/map/MapNode.java
@@ -3,6 +3,7 @@
  */
 package map;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -12,7 +13,56 @@ import java.util.Set;
 public class MapNode 
 {
 	/** The list of edges out of this node */
-	private Set<MapEdge> edges;
+	private HashSet<MapEdge> edges;
+	
+	/** The list of streets that this node is part of */
+	private HashSet<String> streets;
+	
+	/** the latitude and longitude of this node */
+	private GeographicPoint location;
+	
+	public MapNode(GeographicPoint loc)
+	{
+		location = loc;
+		edges = new HashSet<MapEdge>();
+		streets = new HashSet<String>();
+	}
+	
+	public void addStreet(String street)
+	{
+		//XXX might change to boolean
+		streets.add(street);
+	}
+	
+	public void addEdge(MapEdge edge)
+	{
+		//XXX Again, might want to change to boolean
+		edges.add(edge);
+	}
+	
+	/** Returns whether two nodes are equal.
+	 * Nodes are considered equal if their locations are the same, 
+	 * even if their street list is different.
+	 * XXX: Maybe change this?
+	 */
+	public boolean equals(Object o)
+	{
+		if (!(o instanceof MapNode) || (o == null)) {
+			return false;
+		}
+		MapNode node = (MapNode)o;
+		return node.location.equals(this.location);
+	}
+	
+	/** Because we compare nodes using their location, we also 
+	 * use their location for HashCode
+	 * @return The HashCode for this node, which is the HashCode for the 
+	 * underlying point
+	 */
+	public int HashCode()
+	{
+		return location.hashCode();
+	}
 	
 	
 }
