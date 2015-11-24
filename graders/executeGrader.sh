2df6e31f766b1f11c1c4ec578288dcb63e9e8891
Alec Brickner <abrickne@ucsd.edu>
2015-11-24 11:06:49 -0800

    Add module 1 grader

--- graders/executeGrader.sh
+++ graders/executeGrader.sh
@@ -2,18 +2,10 @@
 
 cd /grader
 
-MOD1_PART1_ID="temp"
-MOD1_PART2_ID="temp2"
-MOD2_PART1_ID="temp3"
-MOD2_PART2_ID="mEOEF"
-MOD3_PART1_1_ID="2xj63"
-MOD3_PART1_2_ID="In7Bo"
-MOD3_PART2_ID="p6Yhk"
-MOD4_PART1_ID="3YwBB"
-MOD4_PART2_ID="gjuKe"
-MOD5_PART1_ID="IEm7C"
-MOD5_PART2_ID="pmzXG"
-MOD5_PART3_ID="16fSM"
+MOD1_ID="temp"
+MOD2_ID="temp2"
+MOD3_ID="temp3"
+
 
 while [ $# -gt 1 ]
   do
@@ -35,30 +27,22 @@ while [ $# -gt 1 ]
   shift
 done
 
-if [ "$PARTID" == "$MOD1_PART1_ID" ] || [ "$PARTID" == "$MOD1_PART2_ID" ]; then
-  if [ "$PARTID" == "$MOD1_PART1_ID" ]; then
-    GRADER_DIRECTORY=mod1/part1
-  else
-    GRADER_DIRECTORY=mod1/part2
-  fi
-  FILENAME="document.BasicDocumentGrader"
-  unzip /shared/submission/mod1.zip -d zipfile > /dev/null
+if [ "$PARTID" == "$MOD1_ID" ]; then
+  FILENAME="week1maps.GraphGrader"
+  GRADER_DIRECTORY="mod1"
+  7z e -ozipfile /shared/submission/mod1.zip > /dev/null
   cd zipfile
-  if [ ! -f "BasicDocument.java" ]; then
+  if [ ! -f "GraphAdjList.java" ]; then
+    rm -rf __MACOSX > /dev/null
     cd *
   fi
-  cp * /grader/"$GRADER_DIRECTORY"/document/
+  cp * /grader/"$GRADER_DIRECTORY"/week1maps/
   cd /grader/"$GRADER_DIRECTORY"
-  javac -encoding ISO-8859-1 document/*.java 2>errorfile
-elif [ "$PARTID" == "$MOD2_PART1_ID" ] || [ "$PARTID" == "$MOD2_PART2_ID" ]; then
-  if [ "$PARTID" == "$MOD2_PART1_ID" ]; then
-    GRADER_DIRECTORY=mod2/part1
-    FILENAME="document.EfficientDocumentGrader"
-  else
-    GRADER_DIRECTORY=mod2/part2
-    FILENAME="document.DocumentBenchmarking"
-  fi
-  unzip /shared/submission/mod2.zip -d zipfile > /dev/null
+  javac -encoding ISO-8859-1 week1maps/*.java 2>errorfile
+elif [ "$PARTID" == "$MOD2_ID" ]; then
+  FILENAME="week2example.bleh"
+  GRADER_DIRECTORY="mod2"
+  7z e -ozipfile /shared/submission/mod2.zip > /dev/null
   cd zipfile
   if [ ! -f "EfficientDocument.java" ]; then
     rm -rf __MACOSX > /dev/null
@@ -67,58 +51,8 @@ elif [ "$PARTID" == "$MOD2_PART1_ID" ] || [ "$PARTID" == "$MOD2_PART2_ID" ]; the
   cp * /grader/"$GRADER_DIRECTORY"/document/ 
   cd /grader/"$GRADER_DIRECTORY"
   javac -encoding ISO-8859-1 -sourcepath document document/*.java 2>errorfile
-elif [ "$PARTID" == "$MOD3_PART1_1_ID" ]; then
-  GRADER_DIRECTORY=mod3/part1
-  FILENAME="textgen.MyLinkedListGrader"
-  cp /shared/submission/MyLinkedList.java "$GRADER_DIRECTORY"/textgen
-  cd "$GRADER_DIRECTORY"
-  javac -encoding ISO-8859-1 -cp .:/usr/share/java/junit4.jar textgen/*.java 2>errorfile
-elif [ "$PARTID" == "$MOD3_PART1_2_ID" ]; then
-  GRADER_DIRECTORY=mod3/part1
-  FILENAME="mod3part1.py"
-  cp /shared/submission/MyLinkedListTester.java "$GRADER_DIRECTORY"/textgen
-  cd "$GRADER_DIRECTORY"
-  python "$FILENAME"
-  exit 0
-elif [ "$PARTID" == "$MOD3_PART2_ID" ]; then
-  GRADER_DIRECTORY=mod3/part2
-  FILENAME="textgen.MarkovTextGeneratorGrader"
-  cp /shared/submission/MarkovTextGeneratorLoL.java "$GRADER_DIRECTORY"/textgen
-  cd "$GRADER_DIRECTORY"
-  javac -encoding ISO-8859-1 textgen/*.java 2>errorfile
-elif [ "$PARTID" == "$MOD4_PART1_ID" ]; then
-  GRADER_DIRECTORY=mod4/part1
-  FILENAME="spelling.DictionaryGrader"
-  unzip /shared/submission/mod4part1.zip -d zipfile > /dev/null
-  cd zipfile
-  if [ ! -f "DictionaryLL.java" ]; then
-    cd *
-  fi
-  cp * /grader/mod4/part1/spelling
-  cd /grader/"$GRADER_DIRECTORY"
-  javac -encoding ISO-8859-1 spelling/*.java 2>errorfile
-elif [ "$PARTID" == "$MOD4_PART2_ID" ]; then
-  GRADER_DIRECTORY=mod4/part2
-  FILENAME="spelling.TrieGrader"
-  cp /shared/submission/AutoCompleteDictionaryTrie.java "$GRADER_DIRECTORY"/spelling
-  cd "$GRADER_DIRECTORY"
-  javac -encoding ISO-8859-1 spelling/*.java 2>errorfile
-elif [ "$PARTID" == "$MOD5_PART1_ID" ] || [ "$PARTID" == "$MOD5_PART2_ID" ]; then
-  if [ "$PARTID" == "$MOD5_PART1_ID" ]; then
-    GRADER_DIRECTORY=mod5/part1
-  else
-    GRADER_DIRECTORY=mod5/part2
-  fi
-  FILENAME="spelling.NearbyWordsGrader"
-  cp /shared/submission/NearbyWords.java "$GRADER_DIRECTORY"/spelling
-  cd "$GRADER_DIRECTORY"
-  javac -encoding ISO-8859-1 spelling/*.java 2>errorfile
-elif [ "$PARTID" == "$MOD5_PART3_ID" ]; then
-  GRADER_DIRECTORY=mod5/part3
-  FILENAME="spelling.WPTreeGrader"
-  cp /shared/submission/WPTree.java "$GRADER_DIRECTORY"/spelling
-  cd "$GRADER_DIRECTORY"
-  javac -encoding ISO-8859-1 spelling/*.java 2>errorfile
+elif [ "$PARTID" == "$MOD3_ID" ]; then
+  etc
 else
   echo "{ \"fractionalScore\": 0.0, \"feedback\":\"No partID matched: "$PARTID"\" }"
   exit 1
---
+++ graders/mod1/week1maps/Graph.java
@@ -0,0 +1,97 @@
+package week1maps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/** An abstract class that implements a directed graph. 
+ * The graph may have self-loops, parallel edges. 
+ * Vertices are labeled by integers 0 .. n-1. 
+ * The edges of the graph are not labeled.
+ * Representation of edges is left abstract.
+ * 
+ * @author UC San Diego Intermediate Programming MOOC team
+ *
+ */
+
+public abstract class Graph {
+
+	private int numVertices;
+	private int numEdges;
+	
+	public Graph() {
+		numVertices = 0;
+		numEdges = 0;
+	}
+	
+	public int getNumVertices() {
+		return numVertices;
+	}
+	
+	public int getNumEdges() {
+		return numEdges;
+	}
+	
+	public void addVertex( ) {
+		implementAddVertex();
+		numVertices ++;
+	}
+	
+	public abstract void implementAddVertex();
+
+	public void addEdge( int v , int w ) {
+		//System.out.println("Adding edge between "+v+" and "+w);
+		numEdges ++;
+		if (v < numVertices && w < numVertices) {
+			implementAddEdge( v , w );			
+		}
+		else {
+			throw new IndexOutOfBoundsException();
+		}
+	}
+	
+	public abstract void implementAddEdge(int v , int w);
+
+	public abstract List<Integer> getNeighbors(int v); 
+
+	public String toString() {
+		String s = "Graph with "+numVertices+" vertices and "+numEdges+" edges.\n";
+		s += adjacencyString();
+		return s;
+	}
+
+	public abstract String adjacencyString();
+	
+	public static void main (String[] args) {
+
+		GraphAdjList graphList = new GraphAdjList();
+		graphList.addVertex();
+		graphList.addVertex();
+		graphList.addVertex();
+		graphList.addVertex();
+		graphList.addEdge(0, 0);
+		graphList.addEdge(0, 2);
+		graphList.addEdge(0, 3);
+		graphList.addEdge(2, 3);
+		graphList.addEdge(3, 3);
+		System.out.println(graphList);
+		graphList.getDistance2(0);
+		graphList.getDistance2(3);
+
+		GraphAdjMatrix graphMat = new GraphAdjMatrix();
+		graphMat.addVertex();
+		graphMat.addVertex();
+		graphMat.addVertex();
+		graphMat.addVertex();
+		graphMat.addEdge(0, 0);
+		graphMat.addEdge(0, 2);
+		graphMat.addEdge(0, 3);
+		graphMat.addEdge(2, 3);
+		graphMat.addEdge(3, 3);
+		System.out.println(graphMat);
+		graphMat.getDistance2(0);		
+		graphMat.getDistance2(3);
+	}
+}
