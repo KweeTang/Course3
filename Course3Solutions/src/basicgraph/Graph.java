8cb3e58a9d0c118e0fafb783f6162b07531550a9
miaminnes <miaminneskemp@Mias-MacBook-Pro.local>
2015-11-30 15:14:56 -0800

    Added load method for flight data in MapLoader; added tests in Graph

--- Course3Solutions/src/basicgraph/Graph.java
+++ Course3Solutions/src/basicgraph/Graph.java
@@ -23,13 +23,20 @@ public abstract class Graph {
 
 	private int numVertices;
 	private int numEdges;
-
+	//optional association of String labels to vertices 
+	private Map<Integer,String> vertexLabels;
+	
 	/**
 	 * Create a new empty Graph
 	 */
 	public Graph() {
 		numVertices = 0;
 		numEdges = 0;
+		vertexLabels = null;
+	}
+	
+	public void initializeLabels() {
+		vertexLabels = new HashMap<Integer,String>();
 	}
 	
 	/**
@@ -52,6 +59,48 @@ public abstract class Graph {
 	}
 	
 	/**
+	 * Test whether some vertex in the graph is labeled 
+	 * with a given String label
+	 * @param The String label being checked
+	 * @return True if there's a vertex in the graph with this label; false otherwise.
+	 */
+	public boolean hasVertex(String s)
+	{
+		return vertexLabels.containsValue(s);
+	}
+	
+	/**
+	 * Add label to an unlabeled vertex in the graph.
+	 * @param The index of the vertex to be labeled.
+	 * @param The label to be assigned to this vertex.
+	 */
+	public void addLabel(int v, String s) {
+		if (v < getNumVertices() && !vertexLabels.containsKey(v)) 
+		{
+			vertexLabels.put(v, s);
+		}
+		else {
+			System.out.println("ERROR: tried to label a vertex that is out of range or already labeled");
+		}
+	}
+	
+	public String getLabel(int v) {
+		if (vertexLabels.containsKey(v)) {
+			return vertexLabels.get(v);
+		}
+		else return null;
+	}
+
+	public int getIndex(String s) {
+		for (Map.Entry<Integer,String> entry : vertexLabels.entrySet()) {
+			if (entry.getValue().equals(s))
+				return entry.getKey();
+		}
+		System.out.println("ERROR: No vertex with this label");
+		return -1;
+	}
+	
+	/**
 	 * Report size of edge set
 	 * @return The number of edges in the graph.
 	 */	
@@ -64,10 +113,12 @@ public abstract class Graph {
 	 * have as its index the next available integer.
 	 * Precondition: contiguous integers are used to 
 	 * index vertices.
+	 * @return index of newly added vertex
 	 */
-	public void addVertex() {
+	public int addVertex() {
 		implementAddVertex();
 		numVertices ++;
+		return (numVertices-1);
 	}
 	
 	/**
@@ -135,7 +186,7 @@ public abstract class Graph {
 	public String toString() {
 		String s = "\nGraph with " + numVertices + " vertices and " + numEdges + " edges.\n";
 		s += "Degree sequence: " + degreeSequence() + ".\n";
-		s += adjacencyString();
+		if (numVertices <= 20) s += adjacencyString();
 		return s;
 	}
 
@@ -201,7 +252,7 @@ public abstract class Graph {
 	}
 	
 	public static void main (String[] args) {
-
+/*
 		Graph test1List = buildTestGraph1("list");
 		System.out.println(test1List);
 		test1List.getDistance2(0);
@@ -225,7 +276,11 @@ public abstract class Graph {
 		GraphAdjList graphFromFile = new GraphAdjList();
 		MapLoader.loadMap("data/test.map", graphFromFile);
 		System.out.println(graphFromFile);
-
+*/
+		GraphAdjList airportGraph = new GraphAdjList();
+		System.out.println(airportGraph);
+		MapLoader.loadRoutes("data/routesMaster.dat", airportGraph);
+		System.out.println(airportGraph);
 
 	}
 }
