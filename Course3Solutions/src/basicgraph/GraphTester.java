b49de1b8fde190d8df390edff909338d1a197784
Christine Alvarado <cjalvarado@eng.ucsd.edu>
2015-11-30 12:20:30 -0800

    Added support for one-way map files.  Fixed bugs in graph loader for two-way files on certain conditions.  Added some test files to the data folder in the project.

---
+++ Course3Solutions/src/basicgraph/GraphTester.java
@@ -0,0 +1,30 @@
+package basicgraph;
+
+import geography.GeographicPoint;
+import util.MapLoader;
+
+public class GraphTester {
+
+	public static void main(String[] args)
+	{
+		GraphAdjList listGraph = new GraphAdjList();
+		MapLoader.loadOneWayMap("data/simpletestOneWay.map", listGraph);
+		System.out.println(listGraph.getNumVertices() + " " + listGraph.getNumEdges());
+		System.out.println(listGraph.degreeSequence());
+		
+		GeographicPoint pt1 = new GeographicPoint(32.822, -117.23);
+		GeographicPoint pt2 = new GeographicPoint(32.821, -117.18);
+		
+		MapLoader.testLineInfo(pt1, pt2, "Main");
+		
+		
+		/*
+		GraphAdjMatrix matGraph = new GraphAdjMatrix();
+		MapLoader.loadMap("data/ucsdtest.map", matGraph);
+		System.out.println(matGraph.getNumVertices() + " " + matGraph.getNumEdges());
+		System.out.println(matGraph.degreeSequence());
+		*/
+		
+	}
+	
+}
--- Course3Solutions/src/roadgraph/MapGraph.java
+++ Course3Solutions/src/roadgraph/MapGraph.java
@@ -170,8 +170,7 @@ public class MapGraph {
 	private void addEdge(MapNode n1, MapNode n2, String roadName, String roadType,
 			double length)
 	{
-		// We need to be careful to only add edges once because 
-		// we have no check for duplicate edges
+
 		MapEdge edge = new MapEdge(roadName, roadType, n1, n2, length);
 		edges.add(edge);
 		n1.addEdge(edge);
@@ -272,12 +271,14 @@ public class MapGraph {
 		System.out.print("DONE. \nLoading the map...");
 		HashMap<GeographicPoint,HashSet<RoadSegment>> theRoads = 
 				new HashMap<GeographicPoint,HashSet<RoadSegment>>();
-		MapLoader.loadMap("data/ucsd.map", theMap, theRoads);
+		//MapLoader.loadMap("data/ucsdtest2.map", theMap, theRoads);
+		
+		MapLoader.loadOneWayMap("data/newbury_verysmall_oneway.map", theMap, theRoads);
 		System.out.println("DONE.");
 		
-		//System.out.println("Num nodes: " + theMap.getNumVertices());
-		//System.out.println("Num edges: " + theMap.getNumEdges());
-		theMap.printEdgePointsToFile("data/ucsd.intersections.map");
+		System.out.println("Num nodes: " + theMap.getNumVertices());
+		System.out.println("Num edges: " + theMap.getNumEdges());
+		//theMap.printEdgePointsToFile("data/ucsd.intersections.map");
 		//theMap.printNodes();
 		//theMap.printEdges();
 		
